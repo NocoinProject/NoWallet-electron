685ab02b270bb99425b8306e508b7b557442a725
NocoinProject <41588607+NocoinProject@users.noreply.github.com>
2019-04-11 15:49:14 +0200

    Add files via upload

---
+++ src/js/ws_config.js
@@ -0,0 +1,95 @@
+var config = {};
+
+// self explanatory, your application name, descriptions, etc
+config.appName = 'NoWallet';
+config.appDescription = 'NoCoin NoWallet';
+config.appSlogan = 'A cryptocurrency solution for nocoiners!';
+config.appId = 'org.nocoinproject.nowallet';
+config.appGitRepo = 'https://github.com/turtlecoin/turtle-wallet-electron';
+
+// default port number for your daemon (e.g. TurtleCoind)
+config.daemonDefaultRpcPort = 11898;
+
+// wallet file created by this app will have this extension
+config.walletFileDefaultExt = 'wallet';
+
+// change this to match your wallet service executable filename
+config.walletServiceBinaryFilename = 'nocoin-service';
+
+// version on the bundled service (turtle-service)
+config.walletServiceBinaryVersion = "v0.1.0";
+
+// config file format supported by wallet service, possible values:
+// ini -->  for turtle service (or its forks) version <= v0.8.3
+// json --> for turtle service (or its forks) version >= v0.8.4
+config.walletServiceConfigFormat = "json";
+
+// default port number for your wallet service (e.g. turtle-service)
+config.walletServiceRpcPort = 8070;
+
+// block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
+config.blockExplorerUrl = 'https://explorer.turtlecoin.lol/transaction.html?hash=[[TX_HASH]]';
+
+// default remote node to connect to, set this to a known reliable node for 'just works' user experience
+config.remoteNodeDefaultHost = '84.184.64.22';
+
+
+// remote node list update url, set to null if you don't have one
+// for TRTL:
+// raw list: https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json
+// filtered: https://trtl.nodes.pub/api/getNodes
+config.remoteNodeListUpdateUrl = null
+
+// set to false if using raw/unfiltered node list
+config.remoteNodeListFiltered = true;
+
+// fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
+config.remoteNodeListFallback = [
+  '84.184.64.22:11898',
+
+];
+
+// your currency name
+config.assetName = 'NoCoin';
+// your currency ticker
+config.assetTicker = 'NOC';
+// your currency address prefix, for address validation
+config.addressPrefix = 'No';
+// standard wallet address length, for address validation
+config.addressLength = 97;
+// integrated wallet address length, for address validation. Added length is length of payment ID encoded in base58.
+config.integratedAddressLength = config.addressLength + ((64 * 11) / 8);
+
+// minimum fee for sending transaction
+config.minimumFee = 100;
+// minimum amount for sending transaction
+config.mininumSend = 1;
+// default mixin/anonimity for transaction
+config.defaultMixin = 3;
+// to represent human readable value
+config.decimalPlaces = 0;
+// to convert from atomic unit
+config.decimalDivisor = 10 ** config.decimalPlaces;
+
+// obfuscate address book entries, set to false if you want to save it in plain json file.
+// not for security because the encryption key is attached here
+config.addressBookObfuscateEntries = true;
+// key use to obfuscate address book contents
+config.addressBookObfuscationKey = '79009fb00ca1b7130832a42de45142cf6c4b7f333fe6fba5';
+// initial/sample entries to fill new address book
+config.addressBookSampleEntries = [
+  {
+    name: 'WalletShell Donation',
+    address: 'TRTLv1A26ngXApin33p1JsSE9Yf6REj97Xruz15D4JtSg1wuqYTmsPj5Geu2kHtBzD8TCsfd5dbdYRsrhNXMGyvtJ61AoYqLXVS',
+    paymentId: '',
+  }
+];
+// cipher config for private address book
+config.addressBookCipherConfig = {
+  algorithm: 'aes-256-gcm',
+  saltLenght: 128,
+  pbkdf2Rounds: 10000,
+  pbkdf2Digest: 'sha512'
+};
+
+module.exports = config;
